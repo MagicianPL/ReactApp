bab58fc2982b7dfedbc0e30ca6ee3dbe66e81cad
Michał Sukowski <sukowskimichal@gmail.com>
2021-08-23 18:52:57 +0200

    Im mapping through array of expenses

--- src/components/WrapperExpenses.js
+++ src/components/WrapperExpenses.js
@@ -16,26 +16,11 @@ const WrapperExpenses = ({ arr: expenses }) => {
   return (
     <div className="wrapper-expenses">
       <ExpensesFilter selectedYear={selectedYearHandler} />
-      <ExpenseItem
-        date={expenses[0].date}
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-      />
-      <ExpenseItem
-        date={expenses[1].date}
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-      />
-      <ExpenseItem
-        date={expenses[2].date}
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-      />
-      <ExpenseItem
-        date={expenses[3].date}
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-      />
+
+      {expenses.map(obj => {
+        return <ExpenseItem date={obj.date} title={obj.title} amount={obj.amount} />
+      })}
+      
     </div>
   );
 };
