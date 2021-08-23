346ccc430873aa915a7bbc5c000adab9a501f71d
Michał Sukowski <sukowskimichal@gmail.com>
2021-08-23 22:04:07 +0200

    Filter by year is now working

--- src/components/WrapperExpenses.js
+++ src/components/WrapperExpenses.js
@@ -7,17 +7,22 @@ const WrapperExpenses = ({ arr: expenses }) => {
   //In props is object with arr attribute - arr: array
   //I'm destructuring this object n changes name on expenses
 
-  const [selectedYear, setSelectedYear] = useState("");
+  const [selectedYear, setSelectedYear] = useState("2021");
 
   const selectedYearHandler = (e) => {
     console.log(e.target.value);
     setSelectedYear(e.target.value);
   };
+
+  const filtered = expenses.filter((obj) => {
+    return obj.date.getFullYear().toString() === selectedYear;
+  });
+
   return (
     <div className="wrapper-expenses">
       <ExpensesFilter selectedYear={selectedYearHandler} />
 
-      {expenses.map((obj) => {
+      {filtered.map((obj) => {
         return (
           <ExpenseItem
             key={obj.title}
